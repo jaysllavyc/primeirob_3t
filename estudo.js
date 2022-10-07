cf8069f85412cf764295f8af38d0261bff592e3c
jaysllavyc <110116423+jaysllavyc@users.noreply.github.com>
2022-10-07 09:07:10 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -32,3 +32,20 @@ function mutiplica(){
         }
     }
 }
+
+function total(){
+    let v = document.getElementById("valor").value ;
+    let j = document.getElementById("juros").value ;
+    let t = document.getElementById("messes").value ;
+    let r = 0;
+    for(let i=1; i <= t; i++){
+         r = v * (1 + j/100);
+         document.write("Mes " + i + "valor; " + r + "<br>");
+         v = r;
+        
+    }
+    
+
+    document.write("Resultado: " + r);
+    
+}
